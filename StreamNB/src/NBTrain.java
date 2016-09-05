f4936da2fdcd78df709f2d3fdfe90cc4fd314ad4
lyzmusic <635716260@qq.com>
2016-09-04 23:25:02 -0400

    Init commit

---
+++ StreamNB/src/NBTrain.java
@@ -0,0 +1,8 @@
+// Author: Music
+public class NBTrain{
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
