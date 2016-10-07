4491643b6c11ef28d1c1b03f4243420ab8597d1c
lyzmusic <635716260@qq.com>
2016-10-07 14:22:32 -0400

    - Finished the first version. Seems buggy - TODO: Add bias, debug

--- SGD/src/LR.java
+++ SGD/src/LR.java
@@ -1,30 +1,96 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by MusicLee on 10/6/16.
  */
 public class LR {
-    HashMap<String, HashMap<Integer, Integer>> label_A;
-    HashMap<String, HashMap<Integer, Float>> label_B;
+    HashMap<String, int[]> label_A = new HashMap<String, int[]>();
+    HashMap<String, double[]> label_B = new HashMap<String, double[]>();
+    static double overflow=20;
 
-    public void initialize(String labels){
-        
+    public void initialize(int N, String labels){
+        String[] ls = labels.split(" ");
+        for (String l:ls){
+            label_A.put(l, new int[N]);
+            label_B.put(l, new double[N]);
+        }
+        return;
     }
 
     public void train(BufferedReader in, int N, float lambda, float mu, int max_iter, int train_size) throws IOException {
-
+        // TODO: Add Bias
         String line;
+        int k = 0;
         while ((line = in.readLine()) != null) {
-            // TODO: Handle input line
-            System.out.println(line);
-            break;
+            k += 1;
+            String[] infos = line.split("\t");
+            String[] labels = infos[1].split(",");
+            String doc = infos[2];
+            Vector<String> tokens = tokenizeDoc(doc);
+            HashMap<Integer, Integer> x = new HashMap<Integer, Integer>();
+            for (String token: tokens){
+                int hash = word2hash(N, token);
+                if(x.containsKey(hash)){x.put(hash, x.get(hash)+1);}
+                else{x.put(hash, 1);}
+            }
+            for (String label: label_A.keySet()) {
+                // for each label
+                // if label in labels, y=1; else y=0
+                int[] A = label_A.get(label);
+                double[] B = label_B.get(label);
+                int y = 0;
+                for (String l : labels) {
+                    if (l.equals(label)) y = 1;
+                }
+                double accum = 0;
+                for (int hash : x.keySet()) {
+                    accum += B[hash] * x.get(hash);
+                }
+                double p = sigmoid(-accum);
+                for (int hash : x.keySet()) {
+                    B[hash] *= Math.pow((double) (1 - 2 * lambda * mu), (double) (k - A[hash]));
+                    B[hash] += lambda * (y - p) * x.get(hash);
+                    A[hash] = k;
+                }
+            }
         }
+        // Update at the end
+        for (String label: label_A.keySet()){
+            int[] A = label_A.get(label);
+            double[] B = label_B.get(label);
+            for (int i=0; i<N; i++){
+                B[i] *= Math.pow((double) (1 - 2 * lambda * mu), (double) (k - A[i]));
+            }
+        }
+
+        //for (int i=0;i<N;i++)
+        //System.out.print(label_B.get("Location")[i]);
+    }
+
+    double sigmoid(double score){
+        if (score > overflow) score = overflow;
+        else if (score < -overflow) score = -overflow;
+        double exp = Math.exp(score);
+        return exp/(1+exp);
+    }
+
+    public int word2hash(int N, String word){
+        int id = word.hashCode() % N;
+        if (id < 0) id+= N;
+        return id;
+    }
+
+    public Vector<String> tokenizeDoc(String cur_doc){
+        String[] words = cur_doc.split("\\s+");
+        Vector<String> tokens = new Vector<String>();
+        for (int i=0; i<words.length; i++){
+            words[i] = words[i].replaceAll("\\W", "");
+            if (words[i].length() > 0){
+                tokens.add(words[i]);
+            }
+        }
+        return tokens;
     }
 
     public HashSet<String> readLabels(BufferedReader in) throws IOException {
@@ -42,6 +108,42 @@ public class LR {
         print(label_set);
         print(label_set.size());
         return label_set;
+    }
+
+    void test(String test_file, int N) throws IOException {
+        File fin = new File(test_file);
+        FileInputStream fis = new FileInputStream(fin);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            String[] infos = line.split("\t");
+            String[] labels = infos[1].split(",");
+            String doc = infos[2];
+            Vector<String> tokens = tokenizeDoc(doc);
+            HashMap<Integer, Integer> x = new HashMap<Integer, Integer>();
+            for (String token: tokens){
+                int hash = word2hash(N, token);
+                if(x.containsKey(hash)){x.put(hash, x.get(hash)+1);}
+                else{x.put(hash, 1);}
+            }
+            int count = 0;
+            for (String label:labels){
+                System.out.print(label + ",");
+            }
+            System.out.print(": ");
+            for (String label: label_A.keySet()) {
+                double[] B = label_B.get(label);
+                double accum = 0;
+                for (int hash : x.keySet()) {
+                    accum += B[hash] * x.get(hash);
+                }
+                double p = sigmoid(-accum);
+                if (count == 0) System.out.print(label + "\t" + new Double(p).toString());
+                else System.out.print(',' + label + "\t" + new Double(p).toString());
+                count += 1;
+            }
+            System.out.println();
+        }
     }
 
 
@@ -67,9 +169,10 @@ public class LR {
         //HashSet<String> label_set = lr.readLabels(in);
         LR lr =new LR();
         String labels = "Agent other Organisation TimePeriod Device Activity ChemicalSubstance MeanOfTransportation SportsSeason Biomolecule Work CelestialBody Event Person Species Place Location";
-        lr.initialize(labels);
-        //lr.train(in, N, lambda, mu, max_iter, train_size);
-        //lr.test(test_file)
+        lr.initialize(N, labels);
+        lr.train(in, N, lambda, mu, max_iter, train_size);
+        lr.test(test_file, N);
+
     }
 
     public void print(int i){
--- SGD/src/run.sh
+++ SGD/src/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 javac LR.java
-cat ../../data/dbpedia_16fall/abstract.small.train | java -Xmx128m LR 10000 0.5 0.1 1 44925 test
+cat ../../data/dbpedia_16fall/abstract.tiny.train | java -Xmx128m LR 10000 0.5 0.1 1 44925 ../../data/dbpedia_16fall/abstract.tiny.train
 #java -Xmx128m LR abc 10000 0.5 0.1 1 44925 test
