93ca293782c9491126bb30d44fe25b3f2450a264
lyzmusic <635716260@qq.com>
2016-10-31 19:53:46 -0400

    - Update Readme - Start working on ApproxPageRank.java

---
+++ ApproximatePageRank/src/ApproxPageRank.java
@@ -0,0 +1,27 @@
+/**
+ * Compute page ranks and subsample a graph of local community given a seed.
+ *
+ * Input
+ *   argv[0]: path to adj graph. Each line has the following format
+ *            <src>\t<dst1>\t<dst2>...
+ *   argv[1]: seed node
+ *   argv[2]: alpha
+ *   argv[3]: epsilon
+ *
+ * Output
+ *   Print to stdout. Lines have the following format
+ *     <v1>\t<pagerank1>\n
+ *     <vr>\t<pagerank2>\n
+ *     ...
+ *   Order does NOT matter.
+ */
+
+import java.io.*;
+import java.util.*;
+
+
+public class ApproxPageRank {
+    public static void main(String[] args) throws IOException {
+        // TODO: start your code here
+    }
+}
--- README.md
+++ README.md
@@ -4,9 +4,17 @@ HW1: Streaming Naive Bayes using Hadoop Mapreduce.
 * Directory: StreamNB
 * Intellij Project
 * Java 1.8
+* Hadoop Mapreduce
 
 HW2: Naive Bayes using GuineaPig
 * Python
 * GuineaPig (Which is a python version of PIG developed by the instructor)
+
+HW3: Efficient (Lazy) regularized SGD for Logistic Regression
+* Java 1.8
+
+HW4: Efficient Approximate PageRank
+* Java 1.8
+* Gephi for visualization communities.
 
 
