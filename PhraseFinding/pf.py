9d53cf3df041b358cf85c362ac0410a9f2ae58c3
Yuezhang Li <yuezhanl@unix3.andrew.cmu.edu>
2016-11-26 17:06:45 -0500

    working on pf

--- PhraseFinding/pf.py
+++ PhraseFinding/pf.py
@@ -14,8 +14,21 @@
 """
 
 import sys
+from operator import add
 from pyspark import SparkConf, SparkContext
 
+def fg_word_count(grams, fg_year):
+    grams = grams.filter(lambda (words, decade, count): int(decade) == fg_year).map(lambda (words, decade, count): (words, count))
+    grams = grams.reduceByKey(add)
+    return grams
+
+def bg_word_count(grams, fg_year):
+    grams = grams.filter(lambda (words, decade, count): int(decade) != fg_year).map(lambda (words, decade, count): (words, count))
+    grams = grams.reduceByKey(add)
+    return grams
+
+def total_unique_word_count(grams):
+    return grams.map(lambda (words, decade, count): (words, count)).groupByKey().count()
 
 def main(argv):
     # parse args
@@ -32,8 +45,35 @@ def main(argv):
     conf = SparkConf().setMaster('local[{}]'.format(n_workers))  \
                       .setAppName(argv[0])
     sc = SparkContext(conf=conf)
-
+    
     # TODO: start your code here
+    stopwords = sc.textFile(f_stopwords).cache()
+    stopwords = set(stopwords.collect())
+    # (unigram, decade, count)
+    unigrams = sc.textFile(f_unigrams).map(lambda x:tuple(x.split('\t')))
+    unigrams = unigrams.filter(lambda (unigram, decade, count): unigram not in stopwords)
+    # ((word1, word2), decade, count)
+    bigrams  = sc.textFile(f_bigrams).map(lambda x: x.split('\t'))
+    bigrams  = bigrams.map(lambda (words, decade, count): ((words.split(' ')[0], words.split(' ')[1]), decade, count))
+    bigrams  = bigrams.filter(lambda ((word1, word2), decade, count): word1 not in stopwords and word2 not in stopwords)
+    # foreground word count 
+    fgWordCount = fg_word_count(unigrams, fg_year)
+    bgWordCount = bg_word_count(unigrams, fg_year)
+    fgPhraseCount = fg_word_count(bigrams, fg_year)
+    bgPhraseCount = bg_word_count(bigrams, fg_year)
+    # Total count
+    B = total_unique_word_count(bigrams)
+    U = total_unique_word_count(unigrams)
+    print "Number of unique bigrams", B
+    print "Number of unique unigrams", U
+
+    #res = fgPhraseCount.collect()
+    #print res
+    #print len(res)
+
+    
+    
+    
 
     """ terminate """
     sc.stop()
